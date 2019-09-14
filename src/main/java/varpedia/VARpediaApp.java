376cfe1589a74d7ba1d0839ec1f619d878c8e607
Di Kun Ong <dikuno10two@gmail.com>
2019-09-14 16:34:55 +1200

    Project and Gradle setup

---
+++ src/main/java/varpedia/VARpediaApp.java
@@ -0,0 +1,16 @@
+package varpedia;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class VARpediaApp extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
