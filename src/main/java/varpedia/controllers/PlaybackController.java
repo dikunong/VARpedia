09cd65bbd5606170ce6a09e04d4ae84b4cd8d906
PisuCat <tzag747@aucklanduni.ac.nz>
2019-09-23 19:50:06 +1200

    Added some code into media player (still a few issues to work out with layout)

--- src/main/java/varpedia/controllers/PlaybackController.java
+++ src/main/java/varpedia/controllers/PlaybackController.java
@@ -1,12 +1,18 @@
 package varpedia.controllers;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
+import javafx.scene.media.MediaPlayer.Status;
+import javafx.util.Duration;
 
 import java.io.File;
 
@@ -28,7 +34,13 @@ public class PlaybackController extends Controller {
     private Button exitBtn;
 
     private MediaPlayer _player;
-
+    private Duration _duration;
+	private boolean _actualPaused;
+    
+	private ChangeListener<Number> _timeListener = (ObservableValue<? extends Number> observable, Number oldValue, Number newValue) -> {
+		_player.seek(_duration.multiply(newValue.doubleValue() / 100));
+	};
+    
     @FXML
     private void initialize() {
         // mediaView.setMediaPlayer(player);
@@ -38,16 +50,48 @@ public class PlaybackController extends Controller {
 
         // Tudor's code - adding event listeners???
         // particularly stuff regarding the sliders and MediaView sizing
+    	
+		timeSlider.valueProperty().addListener(_timeListener);
+		timeSlider.addEventFilter(MouseEvent.MOUSE_PRESSED, (MouseEvent e) -> {timeSlider.setValueChanging(true);});
+		timeSlider.addEventFilter(MouseEvent.MOUSE_RELEASED, (MouseEvent e) -> {timeSlider.setValueChanging(false);});
+		timeSlider.setDisable(true);
+		volSlider.setValue(100);
+    	
+    	timeSlider.valueChangingProperty().addListener((ObservableValue<? extends Boolean> obersvable, Boolean oldValue, Boolean newValue) -> {
+			if (newValue) {
+				_player.pause();
+			} else if (!_actualPaused) {
+				_player.play();
+			}
+		});
+
+		volSlider.valueProperty().addListener((ObservableValue<? extends Number> observable, Number oldValue, Number newValue) -> {
+			_player.setVolume(newValue.doubleValue() / 100);
+		});
+		
+		playMedia(new File("video.mp4"));
     }
 
     @FXML
     private void pressPlayButton(ActionEvent event) {
-        // Tudor's code - play.setOnAction()
+		if (_player.getStatus() == Status.PLAYING) {
+			_player.pause();
+			playBtn.setText("|>");
+			_actualPaused = true;
+		} else {
+			_player.play();
+			playBtn.setText("||");
+			_actualPaused = false;
+		}
     }
 
     @FXML
     private void pressExitButton(ActionEvent event) {
-        // Tudor's code - stopMedia()
+		_player.stop();
+		_player.dispose();
+		_player = null;
+		mediaView.setMediaPlayer(null);
+		timeSlider.setDisable(true);
         changeScene(event, "../MainScreen.fxml");
     }
 
@@ -56,6 +100,46 @@ public class PlaybackController extends Controller {
     // let's figure this out in our meeting tomorrow
 
     public void playMedia(File file) {
-        // Tudor's code - playMedia()
-    }
+		Media media = new Media(file.toURI().toString());
+		_player = new MediaPlayer(media);
+		_player.setAutoPlay(true);
+		
+		_player.setOnReady(() -> {
+			_duration = media.getDuration();
+			timeSlider.setDisable(false);
+		});
+		
+		_player.currentTimeProperty().addListener((ObservableValue<? extends Duration> observable, Duration oldValue, Duration newValue) -> {
+			if (_duration != null) {
+				timeSlider.valueProperty().removeListener(_timeListener);
+				timeSlider.setValue((_player.getCurrentTime().toMillis() / _duration.toMillis()) * 100);
+				timeSlider.valueProperty().addListener(_timeListener);
+
+				int count = (int)_player.getCurrentTime().toSeconds();
+				int sec = count % 60;
+				count /= 60;
+				int min = count % 60;
+				count /= 60;
+				int hr = count;
+				count = (int)_duration.toSeconds();
+				int durSec = count % 60;
+				count /= 60;
+				int durMin = count % 60;
+				count /= 60;
+				int durHr = count;
+				
+				if (durHr == 0) {
+					timeLabel.setText(String.format("%d:%02d/%d:%02d", min, sec, durMin, durSec));
+				} else {
+					timeLabel.setText(String.format("%d:%02d:%02d/%d:%02d:%02d", hr, min, sec, durHr, durMin, durSec));
+				}
+			}
+		});
+
+		_player.volumeProperty().addListener((ObservableValue<? extends Number> observable, Number oldValue, Number newValue) -> {
+			volLabel.setText("Vol: " + Math.round(_player.getVolume() * 100) + "%");
+		});
+		
+		mediaView.setMediaPlayer(_player);
+	}
 }
