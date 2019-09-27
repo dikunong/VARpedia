792402506f1e7722c65ae2c6c946a3ce2206f1ef
PisuCat <tzag747@aucklanduni.ac.nz>
2019-09-27 22:40:37 +1200

    Split FFMPEGVideoTask to FlickrTask and FFMPEGVideoTask

--- src/main/java/varpedia/controllers/ChunkAssemblerController.java
+++ src/main/java/varpedia/controllers/ChunkAssemblerController.java
@@ -1,5 +1,6 @@
 package varpedia.controllers;
 
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -8,6 +9,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import varpedia.tasks.FFMPEGVideoTask;
+import varpedia.tasks.FlickrTask;
 
 public class ChunkAssemblerController extends Controller {
 
@@ -30,7 +32,7 @@ public class ChunkAssemblerController extends Controller {
     @FXML
     private Button backBtn;
 
-    private Task<Void> _createTask;
+    private Task<? extends Object> _createTask;
 
     private ExecutorService pool = Executors.newCachedThreadPool();
 
@@ -46,23 +48,50 @@ public class ChunkAssemblerController extends Controller {
         // get Flickr images
         // assemble audio + video using ffmpeg
         // does this stuff happen here or in CreationProgressScreen?
-    	//TODO: Better method
+    	//TODO: Far better method
     	if (_createTask == null) {
-        	_createTask = new FFMPEGVideoTask("cat", creationNameTextField.getText(), 10, new String[] {"Alarm01", "Alarm02", "Alarm03"});
-            _createTask.setOnSucceeded(ev -> {
-                System.out.println("Done");
-                _createTask = null;
-            });
-            _createTask.setOnCancelled(ev -> {
-                System.out.println("Cancel");
-                _createTask = null;
-            });
-            _createTask.setOnFailed(ev -> {
-                System.out.println("Fail");
-                _createTask.getException().printStackTrace();
-                _createTask = null;
-            });	
-        	pool.submit(_createTask);
+    		int imageCount = 10; //numOfImagesSpinner.getValue(); This doesn't seem to work properly
+    		String name = creationNameTextField.getText();
+    		
+    		if (imageCount > 0 && imageCount <= 10) {
+    			FlickrTask flickr = new FlickrTask("cat", imageCount);
+    			_createTask = flickr;
+    			_createTask.setOnSucceeded(ev -> {
+                	try {
+						_createTask = new FFMPEGVideoTask("cat", name, flickr.get(), new String[] {"Alarm01", "Alarm02", "Alarm03"});
+	                	_createTask.setOnSucceeded(ev2 -> {
+		                	System.out.println("Done");
+		                    _createTask = null;
+	                	});
+	                	_createTask.setOnCancelled(ev2 -> {
+	                        System.out.println("Cancel");
+	                        _createTask = null;
+	                    });
+	                    _createTask.setOnFailed(ev2 -> {
+	                        System.out.println("Fail");
+	                        _createTask.getException().printStackTrace();
+	                        _createTask = null;
+	                    });
+	                    pool.submit(_createTask);
+                	} catch (InterruptedException | ExecutionException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+                });
+                _createTask.setOnCancelled(ev -> {
+                    System.out.println("Cancel");
+                    _createTask = null;
+                });
+                _createTask.setOnFailed(ev -> {
+                    System.out.println("Fail");
+                    _createTask.getException().printStackTrace();
+                    _createTask = null;
+                });	
+            	pool.submit(_createTask);
+        	} else {
+        		//TODO: Error
+        		System.out.println("No");
+        	}
     	} else {
     		_createTask.cancel(true);
     	}    	
--- src/main/java/varpedia/tasks/FFMPEGVideoTask.java
+++ src/main/java/varpedia/tasks/FFMPEGVideoTask.java
@@ -1,64 +1,14 @@
 package varpedia.tasks;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
 
 import javax.sound.sampled.AudioFileFormat;
 import javax.sound.sampled.AudioSystem;
-
-import com.flickr4java.flickr.Flickr;
-import com.flickr4java.flickr.FlickrException;
-import com.flickr4java.flickr.REST;
-import com.flickr4java.flickr.photos.Photo;
-import com.flickr4java.flickr.photos.PhotoList;
-import com.flickr4java.flickr.photos.PhotosInterface;
-import com.flickr4java.flickr.photos.SearchParameters;
 
 import javafx.concurrent.Task;
 import varpedia.Command;
 
 public class FFMPEGVideoTask extends Task<Void> {
-
-	private static List<String> search(String term, int count) throws FlickrException {
-		if (count == 0) {
-			return new ArrayList<String>();
-		}
-		
-		String key = null;
-		String secret = null;
-		
-		try (BufferedReader keys = new BufferedReader(new InputStreamReader(FFMPEGVideoTask.class.getResourceAsStream("/varpedia/key.txt"))))
-		{
-			key = keys.readLine();
-			secret = keys.readLine();
-		}
-		catch (IOException e)
-		{
-			e.printStackTrace();
-		}
-		
-		List<String> list = new ArrayList<String>();
-		Flickr f = new Flickr(key, secret, new REST());
-		PhotosInterface photos = f.getPhotosInterface();
-		SearchParameters params = new SearchParameters();
-		params.setText(term);
-		params.setSort(SearchParameters.RELEVANCE);
-		params.setMedia("photos");
-		PhotoList<Photo> photoList = photos.search(params, count, 0);
-			
-		for (Photo p : photoList) {
-			list.add("https://farm" + p.getFarm() + ".staticflickr.com/" + p.getServer() + "/" + p.getId() + "_" + p.getSecret() + ".jpg");
-		}
-		
-		return list;
-	}
 	
 	private String[] _chunks;
 	private String _term;
@@ -75,29 +25,6 @@ public class FFMPEGVideoTask extends Task<Void> {
 	@Override
 	protected Void call() throws Exception {
 
-		List<String> list = search(_term, _images);
-		int id = 0;
-		
-		for (String url : list) {
-			URL source = new URL(url);
-			File dest = new File("appfiles/image" + Integer.toString(id) + ".jpg");
-
-			try (InputStream input = source.openStream(); FileOutputStream output = new FileOutputStream(dest)) {
-				byte[] transfer = new byte[4096];
-				int count;
-				
-				while ((count = input.read(transfer)) != -1) {
-					if (isCancelled()) {
-						return null;
-					}
-					
-					output.write(transfer, 0, count);
-				}
-			}
-			
-			id++;
-		}
-		
 		Command audio = new Command("ffmpeg", "-y", "-f", "concat", "-protocol_whitelist", "file,pipe", "-i", "-", "appfiles/audio.wav");
 		audio.run();
 		
@@ -126,7 +53,7 @@ public class FFMPEGVideoTask extends Task<Void> {
 		float length = file.getFrameLength() / file.getFormat().getFrameRate();
 		Command video;
 		
-		if (id > 0) {
+		if (_images > 0) {
 			video = new Command("ffmpeg", "-y", "-f", "concat", "-protocol_whitelist", "file,pipe", "-i", "-", "-i", "appfiles/audio.wav", "-vf", "scale=w=min(iw*540/ih\\,960):h=min(540\\,ih*960/iw),pad=w=960:h=540:x=(960-iw)/2:y=(540-ih)/2,drawtext=text='" + _term + "':x=(w-text_w)/2:y=(h-text_h)/2:fontsize=72:borderw=2:bordercolor=white:expansion=none", "creations/" + _creation + ".mp4");
 		} else {
 			video = new Command("ffmpeg", "-y", "-f", "lavfi", "-t", Float.toString(length), "-i", "color=color=white:size=960x540", "-i", "appfiles/audio.wav", "-vf", "drawtext=text='" + _term + "':x=(w-text_w)/2:y=(h-text_h)/2:fontsize=72:expansion=none", "creations/" + _creation + ".mp4");
@@ -134,14 +61,14 @@ public class FFMPEGVideoTask extends Task<Void> {
 		
 		video.run();
 		
-		for (int i = 0; i < id; i++) {
+		for (int i = 0; i < _images; i++) {
 			if (isCancelled()) {
 				video.end();
 				return null;
 			}
 			
 			video.writeString("file '" + "appfiles/image" + Integer.toString(i) + ".jpg" + "'");
-			video.writeString("duration " + (length / id));
+			video.writeString("duration " + (length / _images));
 		}
 		
 		video.getProcess().getOutputStream().close();
