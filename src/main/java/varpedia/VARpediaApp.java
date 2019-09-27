cebf811efaac853856f7704e8788950b7682e6fa
Di Kun Ong <dikuno10two@gmail.com>
2019-09-27 13:40:56 +1200

    Enabled list view to load creations in dir

--- src/main/java/varpedia/VARpediaApp.java
+++ src/main/java/varpedia/VARpediaApp.java
@@ -1,6 +1,7 @@
 package varpedia;
 
 import javafx.application.Application;
+/*import javafx.concurrent.Task;*/
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -8,6 +9,9 @@ import javafx.stage.Stage;
 import varpedia.controllers.MainController;
 
 import java.io.IOException;
+/*import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;*/
 
 public class VARpediaApp extends Application {
 
@@ -16,6 +20,25 @@ public class VARpediaApp extends Application {
     public static MainController getMainController() {
     	return _mainController;
     }
+
+    // This is how Tudor managed threads in his A2
+    // Would be good to use this method but currently can't use it as Controllers are static
+    // and you can't reference a non-static method from a static context.
+    // Aaaand we just set MainController to static for our Singleton approach attempt.
+    // Figure this out later.
+
+    /*private ExecutorService pool = Executors.newCachedThreadPool(new ThreadFactory() {
+        @Override
+        public Thread newThread(Runnable r) {
+            Thread t = new Thread(r);
+            t.setDaemon(true);
+            return t;
+        }
+    });
+
+    public void submit(Task<?> task) {
+        pool.submit(task);
+    }*/
 
     @Override
     public void start(Stage primaryStage) {
--- src/main/java/varpedia/controllers/MainController.java
+++ src/main/java/varpedia/controllers/MainController.java
@@ -1,12 +1,18 @@
 package varpedia.controllers;
 
-import javafx.application.Platform;
+import javafx.collections.ObservableList;
+import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.ListView;
+import varpedia.tasks.ListPopulateTask;
+
+import java.io.File;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 public class MainController extends Controller {
 
@@ -16,12 +22,15 @@ public class MainController extends Controller {
     private Button deleteBtn;
     @FXML
     private Button createBtn;
+
+    @FXML
+    private ObservableList<String> creationList;
     @FXML
     private ListView<String> creationListView;
 
     @FXML
     private void initialize() {
-        // stuff
+        populateList();
     }
 
     @FXML
@@ -50,4 +59,37 @@ public class MainController extends Controller {
     public String getCreationFileName() {
     	return "video.mp4";
     }
+
+    private void populateList() {
+        File creationsDir = new File("creations");
+
+        // ensure the creations directory is actually a directory AND exists
+        if (creationsDir.isFile()) {
+            creationsDir.delete();
+        }
+        if (!creationsDir.exists()) {
+            creationsDir.mkdir();
+        }
+
+        Task<List<String>> task = new ListPopulateTask(creationsDir);
+        // VARpediaApp.submit(task);
+        // ^ that doesn't work at the moment, but hopefully in the next few hours it will
+        Thread thread = new Thread(task);
+        thread.start();
+
+        task.setOnSucceeded(event -> {
+            try {
+                List<String> newCreations = task.get();
+                if (newCreations != null) {
+                    //creationList.clear(); - use this later if not adding/removing creations individually
+                    creationList.addAll(newCreations);
+                }
+            } catch (InterruptedException | ExecutionException e) {
+                e.printStackTrace();
+            }
+        });
+
+
+    }
+
 }
