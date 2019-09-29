9be485ee302663b9d02cb6ebdd07474414936444
PisuCat <tzag747@aucklanduni.ac.nz>
2019-09-29 17:13:59 +1300

    Fix video generation with 1-2 images

--- src/main/java/varpedia/tasks/FFMPEGVideoTask.java
+++ src/main/java/varpedia/tasks/FFMPEGVideoTask.java
@@ -1,6 +1,8 @@
 package varpedia.tasks;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.util.List;
 
 import javax.sound.sampled.AudioFileFormat;
@@ -51,23 +53,57 @@ public class FFMPEGVideoTask extends Task<Void> {
 		AudioFileFormat file = AudioSystem.getAudioFileFormat(new File("appfiles/audio.wav"));
 		float length = file.getFrameLength() / file.getFormat().getFrameRate();
 		Command video;
+		boolean catJpgMode = false;
 		
-		if (_images.size() > 0) {
+		if (_images.size() > 2) {
 			video = new Command("ffmpeg", "-y", "-f", "concat", "-protocol_whitelist", "file,pipe", "-i", "-", "-i", "appfiles/audio.wav", "-vf", "scale=w=min(iw*540/ih\\,960):h=min(540\\,ih*960/iw),pad=w=960:h=540:x=(960-iw)/2:y=(540-ih)/2,drawtext=textfile=appfiles/search-term.txt:x=(w-text_w)/2:y=(h*3/4-text_h/2):fontsize=72:borderw=2:bordercolor=white:expansion=none", "-pix_fmt", "yuv420p", "-r", "25", "creations/" + _creation + ".mp4");
+		} else if (_images.size() > 0) {
+			//FFMPEG and MediaPlayer do not work properly on Linux with 1-2 images.
+			video = new Command("ffmpeg", "-y", "-f", "image2pipe", "-framerate", Float.toString(length / 2), "-i", "-", "-i", "appfiles/audio.wav", "-vf", "scale=w=min(iw*540/ih\\,960):h=min(540\\,ih*960/iw),pad=w=960:h=540:x=(960-iw)/2:y=(540-ih)/2,drawtext=textfile=appfiles/search-term.txt:x=(w-text_w)/2:y=(h*3/4-text_h/2):fontsize=72:borderw=2:bordercolor=white:expansion=none", "-pix_fmt", "yuv420p", "-r", "25", "creations/" + _creation + ".mp4");
+			catJpgMode = true;
 		} else {
 			video = new Command("ffmpeg", "-y", "-f", "lavfi", "-t", Float.toString(length), "-i", "color=color=white:size=960x540", "-i", "appfiles/audio.wav", "-vf", "drawtext=textfile=appfiles/search-term.txt:x=(w-text_w)/2:y=(h*3/4-text_h/2):fontsize=72:expansion=none", "-pix_fmt", "yuv420p", "-r", "25", "creations/" + _creation + ".mp4");
 		}
 		
 		video.run();
 		
-		for (int i : _images) {
-			if (isCancelled()) {
-				video.end();
-				return null;
+		if (catJpgMode) {
+			//FFMPEG and MediaPlayer do not work properly on Linux with 1-2 images.
+			int[] imgs = new int[2];
+			
+			if (_images.size() > 1) {
+				imgs[0] = _images.get(0);
+				imgs[1] = _images.get(1);
+			} else {
+				//1 is not enough, it needs 2
+				imgs[0] = _images.get(0);
+				imgs[1] = _images.get(0);
 			}
 			
-			video.writeString("file '" + "appfiles/image" + Integer.toString(i) + ".jpg" + "'");
-			video.writeString("duration " + (length / _images.size()));
+			for (int i : imgs) {
+				try (InputStream in = new FileInputStream("appfiles/image" + Integer.toString(i) + ".jpg")) {
+					byte[] transfer = new byte[4096];
+					int count;
+					
+					while ((count = in.read(transfer)) != -1) {
+						if (isCancelled()) {
+							return null;
+						}
+						
+						video.getProcess().getOutputStream().write(transfer, 0, count);
+					}
+				}
+			}
+		} else {
+			for (int i : _images) {
+				if (isCancelled()) {
+					video.end();
+					return null;
+				}
+				
+				video.writeString("file '" + "appfiles/image" + Integer.toString(i) + ".jpg" + "'");
+				video.writeString("duration " + (length / _images.size()));
+			}
 		}
 		
 		video.getProcess().getOutputStream().close();
